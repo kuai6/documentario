13710657f908a759be2d78ac7bf5c18c36a9b578
kuai6 <kuai6@ya.ru>
2019-11-13 19:44:53 +0700

    continue  API processing

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
+use Application\Di\Container;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Document\Di\Repository\DocumentRepositoryFactory;
+use Document\Di\Service\DocumentServiceFactory;
+use Document\Repository\DocumentRepository;
+use Document\Service\DocumentService;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Mvc\Application;
 use Phalcon\Mvc\Dispatcher;
+
 chdir(dirname(__DIR__));
 error_reporting(E_ALL);
 
@@ -39,43 +47,65 @@ try {
 
     $di->set(
         'dispatcher',
-        function() use ($di) {
-
+        function () use ($di) {
             $evManager = $di->getShared('eventsManager');
 
             $evManager->attach(
-                "dispatch:beforeException",
+                'dispatch:beforeException',
                 /**
-                 * @var $dispatcher Dispatcher
+                 * @return bool|null
+                 *
+                 * @var Dispatcher
                  */
-                function($event, $dispatcher, $exception)
-                {
+                function ($event, $dispatcher, $exception) {
                     switch ($exception->getCode()) {
                         case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
                         case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
                             $dispatcher->forward(
-                                array(
+                                [
                                     'controller' => 'Application\Controller\Error',
                                     'action'     => 'http404',
-                                )
+                                ]
                             );
+
                             return false;
                     }
+
                     return null;
                 }
             );
             $dispatcher = new Dispatcher();
             $dispatcher->setEventsManager($evManager);
+
             return $dispatcher;
         },
         true
     );
+
+    $container = new Container($di);
+    $di->set('container', $container);
+
+    $di->set(Connection::class, DriverManager::getConnection($config->database->toArray()));
+
+    $di->set(DocumentRepository::class, function () use ($container) {
+        $factory = new DocumentRepositoryFactory();
+
+        return $factory->create($container);
+    });
+
+    $di->set(DocumentService::class, function () use ($container) {
+        $factory = new DocumentServiceFactory();
+
+        return $factory->create($container);
+    });
+
     /**
      * Handle the request.
      */
     $application = new Application($di);
     $application->useImplicitView(false);
-    echo $application->handle()->getContent();
+    $response = $application->handle();
+    $response->send();
 } catch (\Exception $e) {
     echo $e->getMessage() . '<br>';
     echo '<pre>' . $e->getTraceAsString() . '</pre>';
